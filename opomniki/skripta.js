a3c2239eaf91a038db151e3bcd676b589179c8d1
Jakob Erzar <jakob.erzar@gmail.com>
2016-03-08 20:37:33 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -17,12 +17,24 @@ window.addEventListener('load', function() {
 	}
 	setInterval(posodobiOpomnike, 1000);
 	
+	var dodajOpomnik = function() {
+		var naziv = document.querySelector("#naziv_opomnika").value;
+		var cas = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value = "";
+		document.getElementById("opomniki").innerHTML += 
+		"<div class='opomnik'><div class='naziv_opomnika'>" + naziv + 
+		"</div><div class='cas_opomnika'> Opomnik čez <span>" + cas + "</span> sekund.</div></div>";
+	};
+	
+	
+	
 	var prijavniGumbClick = function() {
 		var text = document.getElementById("uporabnisko_ime").value;
 		document.getElementById("uporabnik").innerHTML = text;
 		document.querySelector(".pokrivalo").style.display = "none";
-	}
+	};
 	
-	var knof = document.querySelector("#prijavniGumb");
-	knof.addEventListener('click', prijavniGumbClick);
+	document.querySelector("#prijavniGumb").addEventListener('click', prijavniGumbClick);
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
 });
